2bf35ef4e1c7e10a48066fdced6b9a7de21038ae
sdanfa <46697237+sdanfa@users.noreply.github.com>
2026-06-06 06:02:24 -0400

    Set socket mark to 0x1 in common.cpp

--- common.cpp
+++ common.cpp
@@ -862,6 +862,8 @@ int new_listen_socket2(int &fd, address_t &addr) {
     }
     setnonblocking(fd);
     set_buf_size(fd, socket_buf_size);
+    //TEST FWMARK, set to 0x1
+    set_socket_mark(fd, 0x1);
 
     mylog(log_debug, "local_listen_fd=%d\n", fd);
 
@@ -890,6 +892,8 @@ int new_connected_socket2(int &fd, address_t &addr, address_t *bind_addr, char *
 
     setnonblocking(fd);
     set_buf_size(fd, socket_buf_size);
+    //TEST FWMARK, set to 0x1
+    set_socket_mark(fd, 0x1);
 
     mylog(log_debug, "[%s]created new udp_fd %d\n", addr.get_str(), fd);
     int ret = connect(fd, (struct sockaddr *)&addr.inner, addr.get_len());
