8b5126376c62a6078bd5ac47f6d30fcbbe92850a
sdanfa <46697237+sdanfa@users.noreply.github.com>
2026-06-06 05:53:01 -0400

    FIXUP: Change mark_number type to uint32_t in set_socket_mark

--- common.cpp
+++ common.cpp
@@ -646,7 +646,7 @@ int set_buf_size(int fd, int socket_buf_size) {
     return 0;
 }
 
-int set_socket_mark(int fd, unsigned int mark_number)
+int set_socket_mark(int fd, uint32_t mark_number)
 {
     if (setsockopt(sockfd, SOL_SOCKET, SO_MARK, &mark_number, sizeof(mark_number)) < 0) {
         mylog(log_fatal, "SO_MARK Failed");
